fcb69ac9b6b536fc8aceb57ae5633dbad02cd134
Roman Müntener <me@mroman.ch>
2018-06-29 15:34:25 +0200

    + doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// The libtoken package provides an interface and generators
+// to generate various sorts of tokens (or random strings) using
+// the native crypto/rand cryptographically strong random number
+// generator if available. If not available it will use a fall back
+// random number generator. 
+package libtoken
