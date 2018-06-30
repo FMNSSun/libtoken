f7da6ba2caa96c2958e7f1ff6294bb45cf1f0661
Roman Müntener <fmnssun@gmail.com>
2018-06-30 08:50:43 +0200

    * rename

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
-// The libtoken package provides an interface and generators
-// to generate various sorts of tokens (or random strings) using
+// The rndstring package provides an interface and generators
+// to generate various sorts of random strings (or tokens) using
 // the native crypto/rand cryptographically strong random number
 // generator if available. If not available it will use a fall back
 // random number generator. 
-package libtoken
+package rndstring
